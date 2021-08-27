04cf33f31f7b75d7e46a1c9fcbf56e8b78d4ba30
Anton <62949848+icepaq@users.noreply.github.com>
2021-08-27 13:50:26 -0400

    UNTESTED: Started approvals function, reanmed files

--- contracts/CADToken.sol
+++ contracts/CADToken.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./EIC20.sol";
+import "./EIP20.sol";
 
-contract CADToken is EIC20 {
+
+
+contract CADToken is EIP20 {
+    
+    struct ApprovalsType { // For approvals function. User (spender) and amount user is allowed to widthdraw (amount).
+        address spender;
+        uint256 amount;
+    }
+    
+    
     address private owner = msg.sender;
     uint256 private supply = 0;
     
     mapping (address => uint256) bank;
+    
+    mapping (address => ApprovalsType[]) approved; // For approve function
     
     function totalSupply() public view returns(uint256) {
         return supply;
@@ -42,14 +53,24 @@ contract CADToken is EIC20 {
         return true;
     }
     
-    // Unfinished
     function approve(address _spender, uint256 _value) public returns (bool success) {
+        approved[msg.sender].push(ApprovalsType(_spender, _value));
         
+        return true;
     }
     
     // Unfinished
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        ApprovalsType[] memory approvals = approved[_owner];
         
+        uint256 amount = 0;
+        
+        for (uint256 i = 0; i < approvals.length; i++) {
+            if (approvals[i].spender == _spender) {
+                amount = approvals[i].amount;
+            }
+        }
+        
+        return amount;
     }
-
 }
--- contracts/EIP20.sol
+++ contracts/EIP20.sol
@@ -2,20 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 
-contract EIC20 {
-    function name() public view returns(string memory) {
+contract EIP20 {
+    function name() public pure returns(string memory) {
         return "CAD Token";
     }
     
-    function symbol() public view returns(string memory) {
+    function symbol() public pure returns(string memory) {
         return "CADT";
     }
     
-    function decimals() public view returns(uint8) {
+    function decimals() public pure returns(uint8) {
         return 0;
     }
     
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    
 }
