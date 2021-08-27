67f98d2eeb97a01f29ccf30ebab016349335bf10
Anton <62949848+icepaq@users.noreply.github.com>
2021-08-27 18:22:45 -0400

    Changed way approval methods work. Used the logic OpenZeppelin uses.

--- contracts/CADToken.sol
+++ contracts/CADToken.sol
@@ -7,18 +7,18 @@ import "./EIP20.sol";
 
 contract CADToken is EIP20 {
     
-    struct ApprovalsType { // For approvals function. User (spender) and amount user is allowed to widthdraw (amount).
-        address spender;
-        uint256 amount;
-    }
-    
     
     address private owner = msg.sender;
     uint256 private supply = 0;
     
     mapping (address => uint256) bank;
     
-    mapping (address => ApprovalsType[]) approved; // For approve function
+    mapping (address => mapping (address => uint256 )) approved; // For approve function
+    
+    function mint(uint256 _amount) public {
+        supply += _amount;
+        bank[msg.sender] += _amount;
+    }
     
     function totalSupply() public view returns(uint256) {
         return supply;
@@ -40,13 +40,14 @@ contract CADToken is EIP20 {
         return true;
     }
     
-    // Unfinished
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(bank[_from] >= _value);
-        //require(approved[_from] == msg.sender);
+        require(approved[_from][_to] >= _value);
         
         bank[_from] -= _value;
         bank[_to] += _value;
+        
+        approved[_from][_to] -= _value;
         
         emit Transfer(_from, _to, _value);
         
@@ -54,23 +55,12 @@ contract CADToken is EIP20 {
     }
     
     function approve(address _spender, uint256 _value) public returns (bool success) {
-        approved[msg.sender].push(ApprovalsType(_spender, _value));
-        
+        require(bank[msg.sender] >= _value);
+        approved[msg.sender][_spender] = _value;
         return true;
     }
     
-    // Unfinished
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        ApprovalsType[] memory approvals = approved[_owner];
-        
-        uint256 amount = 0;
-        
-        for (uint256 i = 0; i < approvals.length; i++) {
-            if (approvals[i].spender == _spender) {
-                amount = approvals[i].amount;
-            }
-        }
-        
-        return amount;
+        return approved[_owner][_spender];
     }
 }
